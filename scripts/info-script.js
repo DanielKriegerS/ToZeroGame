7097a39c685177dedef3dc6866d2fea2821094c1
dschifel <danielschifelbain@gmail.com>
2024-06-03 20:21:20 -0300

    Created info page and his relateds

---
+++ scripts/info-script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const infoTitle = document.querySelector('#info-title h1');
+
+    let HCtitle = "TÍTULO DE INFO";
+
+    infoTitle.textContent = HCtitle;
+});
