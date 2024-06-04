9658309e1e314d1634f6c40005e452d24bf87150
dschifel <danielschifelbain@gmail.com>
2024-06-04 19:01:14 -0300

    Created basic load message function to read the json

--- scripts/info-script.js
+++ scripts/info-script.js
@@ -1,7 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const infoTitle = document.querySelector('#info-title h1');
+const infoTitle = document.querySelector('#info-title');
+let HCtitle = "Informação_inicial";
 
-    let HCtitle = "TÍTULO DE INFO";
+infoTitle.textContent = HCtitle;
 
-    infoTitle.textContent = HCtitle;
-});
+// Função para carregar o conteúdo do arquivo JSON
+async function loadMessage() {
+    try {
+        const response = await fetch('/assets/dicas.json');
+        if (!response.ok) {
+            throw new Error('Erro ao carregar o arquivo de mensagens');
+        }
+        const data = await response.json();
+        const content = data[HCtitle];
+
+        if (content) {
+            const message = content.message || 'Mensagem não encontrada para este título.';
+            const imageUrl = content.image || '';
+
+            console.log('Image URL:', imageUrl);
+
+            document.getElementById('info-text').innerText = message;
+
+            if (imageUrl) {
+                const imageSection = document.getElementById('image-section');
+                // Cria um novo elemento img
+                const imageElement = document.createElement('img');
+                // Define os atributos src e alt
+                imageElement.src = imageUrl;
+                imageElement.alt = `Imagem relacionada à ${HCtitle}`;
+                imageElement.classList.add('img-fluid'); // Adiciona a classe img-fluid
+                // Adiciona a imagem à seção da imagem
+                imageSection.appendChild(imageElement);
+            }
+        } else {
+            document.getElementById('info-text').innerText = 'Mensagem não encontrada para este título.';
+        }
+    } catch (error) {
+        document.getElementById('info-text').innerText = 'Erro ao carregar a mensagem.';
+        console.error(error);
+    }
+}
+
+loadMessage();
