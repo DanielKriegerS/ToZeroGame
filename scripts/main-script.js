a1d4092155988ef7d4be7f792583eefa95f92bcc
dschifel <danielschifelbain@gmail.com>
2024-06-21 18:09:08 -0300

    Created activate operations flow

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('.links');
+    const op_buttons = document.querySelectorAll('.op-button');
 
     // Função para remover a classe 'active' de todos os links
-    function removeActiveClasses() {
+    function removeActiveClassesFromLinks() {
         links.forEach(link => link.classList.remove('active'));
     }
 
+    // Função para remover a classe 'active' de todos os botões de operação
+    function removeActiveClassesFromButtons() {
+        op_buttons.forEach(button => button.classList.remove('active'));
+    }
+
     // Função para adicionar a classe 'active' ao link clicado
-    function addActiveClass(link) {
-        link.classList.add('active');
+    function addActiveClass(element) {
+        element.classList.add('active');
     }
 
     // Verificar se há um link ativo armazenado no localStorage
@@ -20,16 +26,34 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Verificar se há um botão ativo armazenado no localStorage
+    const activeButtonId = localStorage.getItem('activeButtonId');
+    if (activeButtonId) {
+        const button = document.querySelector(`.op-button[data-id="${activeButtonId}"]`);
+        if (button) {
+            addActiveClass(button);
+        }
+    }
+
     links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            removeActiveClasses(); 
-            addActiveClass(link); 
+        link.addEventListener('click', () => {
+            removeActiveClassesFromLinks();
+            addActiveClass(link);
 
             localStorage.setItem('activeLinkId', link.getAttribute('data-id'));
 
             setTimeout(() => {
                 window.location.href = link.href;
             }, 100);
+        });
+    });
+
+    op_buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            removeActiveClassesFromButtons();
+            addActiveClass(button);
+
+            localStorage.setItem('activeButtonId', button.getAttribute('data-id'));
         });
     });
 });
