235c0862b1db8b9f687df28c4b8629fc5e45bbc4
dschifel <danielschifelbain@gmail.com>
2024-06-21 20:02:34 -0300

    Removed op buttons functions from main script

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('.links');
-    const op_buttons = document.querySelectorAll('.op-button');
-    const cancelButton = document.getElementById('cancel-operation');
-    const executeButton = document.getElementById('execute-operation');        
-    const restartButton = document.getElementById('restart-game');
 
+   
     // Função para remover a classe 'active' de todos os links
     function removeActiveClassesFromLinks() {
         links.forEach(link => link.classList.remove('active'));
-    }
-
-    // Função para remover a classe 'active' de todos os botões de operação
-    function removeActiveClassesFromButtons() {
-        op_buttons.forEach(button => button.classList.remove('active'));
     }
 
     // Função para adicionar a classe 'active' ao link clicado
@@ -29,15 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Verificar se há um botão ativo armazenado no localStorage
-    const activeButtonId = localStorage.getItem('activeButtonId');
-    if (activeButtonId) {
-        const button = document.querySelector(`.op-button[data-id="${activeButtonId}"]`);
-        if (button) {
-            addActiveClass(button);
-        }
-    }
-
     links.forEach(link => {
         link.addEventListener('click', () => {
             removeActiveClassesFromLinks();
@@ -49,29 +32,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 window.location.href = link.href;
             }, 100);
         });
-    });
-
-    op_buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            removeActiveClassesFromButtons();
-            addActiveClass(button);
-
-            localStorage.setItem('activeButtonId', button.getAttribute('data-id'));
-        });
-    });
-    
-
-    if (cancelButton && executeButton && restartButton) {
-        cancelButton.addEventListener('click', () => {
-            removeActiveClassesFromButtons();
-        });
-       
-        executeButton.addEventListener('click', () => {
-            removeActiveClassesFromButtons();
-        });
-
-        restartButton.addEventListener('click', () => {
-            removeActiveClassesFromButtons();
-        });
-    }        
+    });  
 });
