feffd22fd873903ccdda3cf2dd6932386b58969e
dschifel <danielschifelbain@gmail.com>
2024-07-04 21:22:04 -0300

    Created plus and minus functions

--- scripts/config-page-script.js
+++ scripts/config-page-script.js
@@ -1,15 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
-   // Carga dos elementos do DOM   
-   const checkbox = document.getElementById('tempo');
-   const clock = document.getElementById('clock');
+    // Carga dos elementos do DOM   
+    const checkbox = document.getElementById('tempo');
+    const clock = document.getElementById('clock');
 
-   // Adiciona evento de mudança ao checkbox
-   checkbox.addEventListener('change', () => {
-       if (checkbox.checked) {
+    const quantity = document.getElementById('quantity');
+    const plus = document.getElementById('plus');
+    const minus = document.getElementById('minus');
+
+    localStorage.setItem('quantity', 3);
+
+    // Adiciona evento de mudança ao checkbox
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
            clock.classList.add('active');
-       } else {
+        } else {
            clock.classList.remove('active');
        }
-   });
+    });
+
+    // Adição de listenners
+    plus.addEventListener('click', function() {
+        plusOneNumber();
+    });  
+
+    minus.addEventListener('click', function(){
+        minusOneNumber();
+    })
+
+    // Efetua operações de mais e menos
+    function plusOneNumber () {
+        let actualQuantity = parseInt(localStorage.getItem('quantity'), 10);
+
+        if(actualQuantity < 4) {
+            actualQuantity += 1;
+            localStorage.setItem('quantity', actualQuantity);
+            console.log(actualQuantity);
+        } else {
+            console.log('maximo atingido');
+        }
+    }
+
+    function minusOneNumber () {
+        let actualQuantity = parseInt(localStorage.getItem('quantity'), 10);
+
+        if(actualQuantity == 3) {
+            console.log('minimo atingido');
+        } else {
+            actualQuantity -= 1;
+            localStorage.setItem('quantity', actualQuantity);
+            console.log(actualQuantity);
+        }
+    }
 
 });
